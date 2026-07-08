c653ee3f64bbbe78322c978a102f4c562258ef2f
tasuku <gaojintasuku@gmail.com>
2026-07-09 00:10:34 +0900

    feat/#19-会員登録、ログインに係るエラーメッセージの設定

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -32,6 +32,16 @@ class CreateNewUser implements CreatesNewUsers
                 Rule::unique(User::class),
             ],
             'password' => $this->passwordRules(),
+        ], [
+            'name.required' => 'お名前を入力してください',
+            'name.max' => 'お名前は255文字以内で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'email.max' => 'メールアドレスは255文字以内で入力してください',
+            'email.unique' => '指定されたメールアドレスは既に登録済みです',
+            'password.required' => 'パスワードを入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => 'パスワードと一致しません',
         ])->validate();
 
         return User::create([
