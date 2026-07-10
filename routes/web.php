cc774fcfb7ef98e4fb7cb053c1a239a013bb5ef7
tasuku <gaojintasuku@gmail.com>
2026-07-10 09:39:26 +0900

    feat/#21-ルーティング修正-ログアウトリダイレクト、リソースルートの並び順

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+// ログアウト後のリダイレクト先を指定
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/books');
 });
 
-// 公開ルート
-// 書籍一覧表示、詳細表示
-Route::resource('books', BookController::class)
-    ->only(['index', 'show']);
-
-// 認証済みユーザーのみアクセス可能なルート
+// 認証ルート
 Route::middleware('auth')->group(function () {
     // 書籍の作成、編集、削除
     Route::resource('books', BookController::class)
@@ -24,3 +20,21 @@ Route::middleware('auth')->group(function () {
             'destroy',
         ]);
 });
+
+// 公開ルート
+// 書籍一覧表示、詳細表示
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+// 仮ルート設置
+Route::get('/ranking', function () {
+    return view('ranking.index');
+})->name('ranking.index');
+
+Route::get('/favorites', function () {
+    return view('favorites.index');
+})->name('favorites.index');
+
+Route::get('/genres', function () {
+    return view('genres.index');
+})->name('genres.index');
