3a7411b39e15b1af3bb8d286d0985381e4e0e17d
tasuku <gaojintasuku@gmail.com>
2026-07-13 11:02:03 +0900

    feat/#31-APIコントローラーの実装

--- app/Http/Requests/Api/V1/IndexBookRequest.php
+++ app/Http/Requests/Api/V1/IndexBookRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Api\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BookIndexRequest extends FormRequest
+class IndexBookRequest extends FormRequest
 {
     public function authorize(): bool
     {
